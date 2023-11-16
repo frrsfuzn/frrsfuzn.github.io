a900acfb48ac63d8a026a79179f35357a22d2559
frrsfuzn <mochfarrasfauzan@gmail.com>
2023-11-16 13:32:46 +0700

    add font weight to heading

--- src/utils/mdx.tsx
+++ src/utils/mdx.tsx
@@ -1,7 +1,7 @@
 export const components = {
-  h1: ({ children }) => <h1 className="text-3xl mb-5">{children}</h1>,
-  h2: ({ children }) => <h2 className="text-2xl mb-4">{children}</h2>,
-  h3: ({ children }) => <h3 className="text-xl mb-3">{children}</h3>,
+  h1: ({ children }) => <h1 className="text-3xl font-bold mb-5">{children}</h1>,
+  h2: ({ children }) => <h2 className="text-2xl font-semibold mb-4">{children}</h2>,
+  h3: ({ children }) => <h3 className="text-xl font-semibold mb-3">{children}</h3>,
   h4: ({ children }) => <h4 className="text-lg mb-2">{children}</h4>,
   p: ({ children }) => <p className="mb-4">{children}</p>,
   ul: ({ children }) => <ul className="list-disc list-inside">{children}</ul>,
